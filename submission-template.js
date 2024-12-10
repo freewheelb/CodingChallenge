9998298eac3b2ce83ec941d66f60ce5068b7a23a
Stephen Crowe <stephencrowe@live.com>
2024-12-10 05:53:32 -0800

    Update submission-template.js

--- submission-template.js
+++ submission-template.js
@@ -1,62 +1,53 @@
-// QUESTION 1 - 4 POINTS
 const findSum = function(array) {
-    // Simple reduce function to sum the array
+    // Function Body
     let sum = array.reduce((a, b) => a + b, 0);
+
+    // Return Statement
        return sum;
-    };
+};
 
-// QUESTION 2
+const findFrequency = function(array) {
+  // your code here - don't forget to return an object!
+};
 
-
-
-// QUESTION 3 - 4 POINTS
-// Function Body
 const isPalindrome = function(str) {
-   let splitStr = str.split('');
+  // Function Body
+  let splitStr = str.split('');
    let revStr = splitStr.reverse();
    let joinStr = revStr.join('');
-   
-   // Return Statement
+
+  // Return statement
    if (str === joinStr){
       return true;
    }
       return false;
-   };
+};
 
-   // Split string and put into array
-   // reverse it
-   // joint it back together
-   // compare it to the original
-   // if the same, return true.
-   // your code here - don't forget to return a boolean!
-
-
-// QUESTION 4 - 3 POINTS
 const largestPair = function(array) {
-   let largestNum = 0;
-   for (let n = 0; n < array.length; n++) {
-     
-     const element = (array[n] + (array[n + 1]));
-     if (element > largestNum) {
-       largestNum = element;
-     }
-   };
- return largestNum;
- };
+  // Function Body
+  let largestNum = 0;
+  for (let n = 0; n < array.length; n++) {
+    
+    const element = (array[n] + (array[n + 1]));
+    if (element > largestNum) {
+      largestNum = element;
+    }
+  };
+  // Return Statement
+return largestNum;
+};
 
-
-//  QUESTION 5 - 3 POINTS
 const removeParenth = function(str) {
-   // Function Body - Return index of requested characters & modifies original string
-     let indexOne = str.indexOf('(');
-     let indexTwo = str.indexOf(')');
-     let edit = str.slice(indexOne, indexTwo + 1);
-     let newString = str.replace(edit, '');
-   
-     // Return Statement
-     return newString;
-   };
+ // Function Body - Return index of requested characters & modifies original string
+ let indexOne = str.indexOf('(');
+ let indexTwo = str.indexOf(')');
+ let edit = str.slice(indexOne, indexTwo + 1);
+ let newString = str.replace(edit, '');
 
+ // Return Statement
+ return newString;
+};
 
-
-
+const scoreScrabble = function(str) {
+  // your code here - don't forget to return a number!
+};
